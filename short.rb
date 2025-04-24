e586fb7cd9993e927dd049d632f0dc2bad13f067
Idar V <IdarV@users.noreply.github.com>
2025-04-24 16:19:56 +0200

    Update short.rb

--- short.rb
+++ short.rb
@@ -1,1 +1,1 @@
-p ARGV.map{|e|e.gsub(/[^([a-z][æøå])\s]/i,'').downcase}.sort_by{|e|e.chars.sum{|c|[*('a'..'z'),'æ','ø','å'].find_index(c)}}.join(" ").capitalize
+p ARGV.map{|e|e.gsub(/[^([a-z][æøå])\s]/i,'').downcase}.sort_by{|e|e.chars.sum{|c|[*('a'..'z'),'æ','ø','å'].find_index(c)+1}}.join(" ").capitalize
